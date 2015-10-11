2eb0a5a305b722ae38ee83d3cc92352d2dc5e7c9
Andrea Terzani <andrea.terzani@gmail.com>
2015-10-11 18:52:58 +0200

    first

--- client/reservation/controllers/ReservationList.js
+++ client/reservation/controllers/ReservationList.js
@@ -4,23 +4,40 @@
       function ($scope, $meteor) {
 
         var date = new Date();
+          $scope.earnings=0;
+          $scope.room_nights=0;
 
 
-        $scope.year=date.getFullYear();
+          $scope.year=date.getFullYear();
           $scope.month=date.getMonth();;
 
-        $meteor.subscribe('confirmed_reservation');
+
+          $meteor.subscribe('confirmed_reservation');
 
           $meteor.autorun($scope, function() {
 
-                var m =parseInt($scope.getReactively('month'));
-              var y=parseInt($scope.getReactively('year'));
+              var m =parseInt($scope.getReactively('month'));
+              var y=parseInt($scope.year);
 
               $scope.reservations = $meteor.collection(function () {
                   return Reservations.find({
                       'date_arrival': {$gte: new Date(y,m,1),
                                         $lt: new Date(y,m+ 1, 1)}
-                  });
+                  })
+              });
+
+          });
+
+          $meteor.autorun($scope, function() {
+              $scope.getReactively('reservations');
+
+              $scope.earnings=0;
+              $scope.room_nights=0;
+
+              $scope.reservations.forEach(function (res) {
+
+                  $scope.earnings += parseFloat(res.amount);
+                  $scope.room_nights += parseInt(res.roomnight);
               });
           });
 
--- client/routes.js
+++ client/routes.js
@@ -132,6 +132,6 @@ angular.module("meteorbeb")
                     }
                 });
 
-            $urlRouterProvider.otherwise("/login    ");
+            $urlRouterProvider.otherwise("/reservations");
         }]);
 
